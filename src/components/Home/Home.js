964e4899a3ff5aa6d31f7a89b6653ade1fa2edb7
Rikcylolo <rikcypadang@gmail.com>
2023-01-23 15:00:12 +0700

    Update Home.js

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -16,7 +16,7 @@ function Home() {
               <h1 style={{ paddingBottom: 15 }} className="heading">
                 Halo Semuanya!{" "}
                 <span className="wave" role="img" aria-labelledby="wave">
-                  👋🏻 adsdasd
+                  👋🏻 
                 </span>
               </h1>
 
