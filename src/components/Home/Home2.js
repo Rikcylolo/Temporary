595561f29b314c46491538b002614807091706dc
Rikcylolo <rikcypadang@gmail.com>
2023-01-29 20:05:44 +0700

    Update Home2.js

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -89,7 +89,7 @@ function Home2() {
                   <AiOutlineTwitter />
                 </a>
               </li>
-               <li className="social-icons">
+               {/* <li className="social-icons">
                 <a
                   href="https://www.linkedin.com/in/soumyajit4419/"
                   target="_blank"
@@ -98,7 +98,7 @@ function Home2() {
                 >
                   <FaLinkedinIn />
                 </a>
-              </li> 
+              </li>  */}
               <li className="social-icons">
                 <a
                   href="https://www.instagram.com/royantsrg/"
