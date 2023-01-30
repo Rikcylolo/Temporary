1798bf6b5b292735b0fcb2adf0dbfe745d8cc0ff
Rikcylolo <rikcypadang@gmail.com>
2023-01-30 13:09:21 +0700

    Resume done..

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,14 +2,14 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/RikcyCV.pdf";
+import pdf from "../../Assets/RikcyCV.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
 pdfjs.GlobalWorkerOptions.workerSrc = `//cdnjs.cloudflare.com/ajax/libs/pdf.js/${pdfjs.version}/pdf.worker.min.js`;
 
 const resumeLink =
-  "https://github.com/Rikcylolo/Temporary/raw/main/src/Assets/RikcyCV.pdf";
+  "https://raw.githubusercontent.com/Rikcylolo/Temporary/68011f3d82f5bc69ca94d29d1092204100b2929e/src/Assets/RikcyCV.pdf";
 
 function ResumeNew() {
   const [width, setWidth] = useState(1200);
@@ -33,6 +33,7 @@ function ResumeNew() {
             &nbsp;Download CV
           </Button>
         </Row>
+
 
         <Row className="resume">
           <Document file={resumeLink} className="d-flex justify-content-center">
