0fcd8aa2c27f43b4ff636972868e849ba00ca0cb
yuliasidabariba <yuliasidabariba@gmail.com>
2023-01-31 22:36:16 +0700

    Update History2.js
    Update history organisasi dan pekerjaan

--- src/components/History/History2.js
+++ src/components/History/History2.js
@@ -19,67 +19,32 @@ function History2() {
                <span className="purple"> PENDIDIKAN </span> 
             </h1>
             <p className="home-about-body">
-              *SD SANTO YOSEF SIDIKALANG  <br></br> *SMP NEGERI 1 SIDIKALANG
+              SD SANTO YOSEF SIDIKALANG  <br></br> SMP NEGERI 1 SIDIKALANG
               <br />
-              *SMA NEGERI 2 SIDIKALANG <br></br> *UNIVERSITAS MIKROSKIL 
+              SMA NEGERI 2 SIDIKALANG <br></br> UNIVERSITAS MIKROSKIL 
               <br />
               
+            </p>
+            <h1 style={{ fontSize: "2.6em" }}>
+               <span className="purple"> <br/><br/>ORGANISASI </span> 
+            </h1>
+            <p className="home-about-body">
+              Anggota Pemuda Pancasia (PP)<br></br> Komunitas Pecinta Kucing Medan
+              <br />
+            
+              
+            </p>
+            <h1 style={{ fontSize: "2.6em" }}>
+               <span className="purple"> <br/><br/>PEKERJAAN </span> 
+            </h1>
+            <p className="home-about-body">
+              Part time barista Starbucks
             </p>
           </Col>
           <Col md={4} className="myAvtar">
             <Tilt>
               <img src={myImg} className="img-fluid" alt="boy.jpg" />
             </Tilt>
-          </Col>
-        </Row>
-        <Row>
-          <Col md={12} className="home-about-social">
-            <h1>IKUTI SAYA DI</h1>
-            <p>
-              Jangan ragu untuk <span className="purple">tehubung </span>dengan saya
-            </p>
-            <ul className="home-about-social-links">
-              {/* <li className="social-icons">
-                <a
-                  href="https://github.com/soumyajit4419"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiFillGithub />
-                </a>
-              </li> */}
-              <li className="social-icons">
-                <a
-                  href="https://www.twitter.com/Lync09"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <AiOutlineTwitter />
-                </a>
-              </li>
-              {/* <li className="social-icons">
-                <a
-                  href="https://www.linkedin.com/in/soumyajit4419/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour  home-social-icons"
-                >
-                  <FaLinkedinIn />
-                </a>
-              </li> */}
-              <li className="social-icons">
-                <a
-                  href="https://www.instagram.com/royantsrg/"
-                  target="_blank"
-                  rel="noreferrer"
-                  className="icon-colour home-social-icons"
-                >
-                  <AiFillInstagram />
-                </a>
-              </li>
-            </ul>
           </Col>
         </Row>
       </Container>
